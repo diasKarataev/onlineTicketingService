7426dd1e5bdfee705d77ff1fa56d240206e98318
diasKarataev <123745326+diasKarataev@users.noreply.github.com>
2023-02-01 13:53:00 +0600

    Initializing a project

---
+++ src/main/java/com/example/onlineTicketingService/OnlineTicketingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineTicketingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlineTicketingService/OnlineTicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineTicketingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTicketingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
